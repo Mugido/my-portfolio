c4d7e5bcfe1ff947ea4186cbdc726d8b0a0207b6
Mugido <sirmugido707@gmail.com>
2024-11-11 17:36:24 +0100

    My Second Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,23 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6 ">
-     <div className="flex flex-shrink-0 items-center">
-      <img className="mx-2 w-10" src={logo} alt="logo" />
-     </div>
-     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <FaLinkedin />
-      <FaGithub />
-      <FaSquareXTwitter />
-      
-     </div>
-      </nav>
-  )
-}
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/uchechukwu-okoro-47187b2a1/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com//Mugido" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://twitter.com/Okorotechgenius" target="_blank" rel="noopener noreferrer">
+          <FaSquareXTwitter />
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
